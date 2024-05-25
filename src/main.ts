3bb60cd7dfd10b32e2f6459320eb0042600e919a
ViniciusFialhus <contatoviniciusfialhu@gmail.com>
2024-05-25 07:45:54 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'process'
-require('dotenv').config();
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000
   app.enableCors({
     origin: 'https://socialspark-fe.vercel.app',
     methods: ['GET', 'POST'],
     credentials: true
   });
-  await app.listen(process.env.PORT, '0.0.0.0');
+  await app.listen(port);
 }
 
 bootstrap();
